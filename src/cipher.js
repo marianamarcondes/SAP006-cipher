4ff6bc65385fcca4d8c84ee3a47ea90707e80403
Mariana Marcondes <mari.marcondes@outlook.com>
2021-06-04 23:16:44 -0300

    JS code implementation - accepting all characters

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,38 @@
-const cipher = {
-  // ...
-};
+const characters = 32;
+const maxCharacters = 237;
 
+const cipher = {
+encode: function(offset, string){
+  let message = "";
+
+    for (let i = 0; i < string.length; i++) {
+    let code = string.charCodeAt(i);
+    if(code >= characters && code <= maxCharacters){
+        message += String.fromCharCode((code - characters + offset) %205 + characters);}
+      else { code += string.charAt(i);
+      }
+      }
+    return message;
+    },
+    
+decode: function(offset, string){
+  let message = "";
+
+    for (let i = 0; i < string.length; i++) {
+    let code = string.charCodeAt(i);
+    if(code >= characters && code <= maxCharacters){
+        message += String.fromCharCode((code - characters - offset) %205 + characters);}
+      else { code += string.charAt(i);
+           }
+           }
+    return message;
+    }
+    }
+                                                                           
+              
+         
 export default cipher;
+
+  
+
+  
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const codeButton = document.getElementById("encrypt");
+    function encryptMessage(e) {
+        e.preventDefault();
+        const offset = Number(document.getElementById("offset").value);
+        let string = document.getElementById("text").value;
+        const encrypted = cipher.encode(offset, string);
+            document.getElementById("code").value = encrypted; 
+                                }
+    codeButton.addEventListener("click", encryptMessage);
+
+const decButton = document.getElementById("decrypt");
+    function decryptMessage(e) {
+        e.preventDefault();
+        const offset = Number(document.getElementById("offset").value);
+        let string = document.getElementById("code").value;
+        const decrypted = cipher.decode(offset, string);
+            document.getElementById("text").value = decrypted;
+    }
+    decButton.addEventListener("click", decryptMessage);
+
+//console.log(cipher);
