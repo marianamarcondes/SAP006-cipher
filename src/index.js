35a610585524790132c38fb3c6eea763d6df6d49
mariana marcondes <71853261+marianamarcondes@users.noreply.github.com>
2021-06-11 19:03:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import cipher from './cipher.js';
 const codeButton = document.getElementById("encrypt"); 
     function encryptMessage() { 
         let offset = Number(document.getElementById("offset").value); 
-<<<<<<< HEAD
-=======
+
            if (offset < 0) {
             offset = Math.abs(offset);}   
->>>>>>> 5be63f48f43c09d82d3c5775fe913a269dea8f15
+
         let string = document.getElementById("text").value;      
         const encrypted = cipher.encode(offset, string); 
             document.getElementById("code").value = encrypted; 
